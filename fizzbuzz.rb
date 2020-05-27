197117627501843c4e2522d10483937ca580e196
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-27 22:37:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,31 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+
   int = int.to_i
+
   if int % 3 == 0
+
     int = "Fizz"
-  if int % 5 == 0
-    int = "Buzz"
-  if int % 15 == 0
-    int = "FizzBuzz"
-  else
-    int = "nil"
+
   end
+
+  if int % 5 == 0
+
+    int = "Buzz"
+
+  end
+
+  if int % 15 == 0
+
+    int = "FizzBuzz"
+
+  if !((int % 3 == 0) || (int % 5 == 0) || (int % 15 == 0))
+
+    int = "nil"
+
+  end
+
   return int
+
 end
