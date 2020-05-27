f2a468e4dc4981b9055beb1ad26ee113cdb5cb27
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-27 22:27:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuz(int)
+def fizzbuzz(int)
   int = int.to_i
   if int % 3 == 0
     int = "Fizz"
