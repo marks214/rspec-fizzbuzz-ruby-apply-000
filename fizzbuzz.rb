422df4b2bd9d71074071fcef0047aa42942a2fe7
Aimee Oz <aimee.oz.x@gmail.com>
2020-05-27 22:23:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuz(int)
+  int = int.to_i
+  if int % 3 == 0
+    puts "fizz"
+  elsif int % 5 == 0
+    puts "buzz"
+  elsif int % 15 == 0
+    puts "fizzbuz"
+  else 
+    puts int
+  end
+end
+    
